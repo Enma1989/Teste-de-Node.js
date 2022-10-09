20a73e1f58bfb39ae18fcc392ac5e71bdb2af6d1
‘Enmanuel <mimo.lds.07@gmail.com>
2022-10-08 21:51:21 -0300

    meu primeiro commit

---
+++ modules/fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+
+// Criar Pasta
+ /*fs.mkdir(path.join(__dirname, '/test'), (error) => {
+    if (error) {
+        return console.log('Erro:  ', error);
+    }
+
+    console.log("Pasta criada com sucesso!");
+});
+
+// Criar um arquivo
+fs.writeFile(path.join(__dirname, 'teste', 'test.html'), 'hello node!', (error) => {
+    if (error) {
+        return console.log('Erro: ', error)
+    }
+
+    console.log('Arquivo criado com sucesso!')
+})*/
